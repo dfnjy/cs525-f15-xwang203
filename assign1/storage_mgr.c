54fd851839e296de9b6904edd1b25f3ab9462445
xwang203 <wangxiao1992@gmail.com>
2015-08-30 19:35:29 -0500

    writing blocks to page file first finished

--- assign1/storage_mgr.c
+++ assign1/storage_mgr.c
@@ -8,12 +8,16 @@
 RC createPageFile (char *fileName)
 {
     FILE *pFile = fopen(fileName,"w");
-    char str[PAGE_SIZE];
-    memset(str,'\0',PAGE_SIZE); //generate a page full of '\0'
-    int total_pages=0; //total pages to write at the beginning
-    fprintf(pFile,"%d\n",total_pages);
+    
+    SM_PageHandle str = (char *)malloc(PAGE_SIZE); //apply for a page
+    memset(str,'\0',PAGE_SIZE); //fill a page full of '\0'
+    
+    fprintf(pFile,"%d\n",0); //total_pages=0, total_pages is an offset
     fwrite(str, sizeof(char), PAGE_SIZE, pFile);
     fclose(pFile);
+    
+    free(str);
+    str=NULL;
     return RC_OK;
 }
 
@@ -40,7 +44,9 @@ RC closePageFile (SM_FileHandle *fHandle)
     if (fail)
         return RC_FILE_HANDLE_NOT_INIT;
     
+    free(fHandle->fileName);
     fHandle->fileName= NULL;
+    free(fHandle->mgmtInfo);
     fHandle->mgmtInfo= NULL;
     return RC_OK;
 }
@@ -54,8 +60,68 @@ RC destroyPageFile (char *fileName)
     return RC_OK;
 }
 
+/* writing blocks to a page file */
+//sizeofchar=1 sizeofint=4
+RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage)
+{
+    if (pageNum > (fHandle->totalNumPages) || pageNum<0)
+        return RC_WRITE_FAILED;
+    
+    int fail = fseek(fHandle->mgmtInfo, 5+pageNum*PAGE_SIZE, SEEK_SET);
+    if (fail)
+        return RC_WRITE_FAILED;
+    
+    fwrite(memPage, sizeof(char), PAGE_SIZE, fHandle->mgmtInfo);
+    fHandle->curPagePos = pageNum;
+    return RC_OK;
+}
+
+RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage)
+{
+    return writeBlock(fHandle->curPagePos, fHandle, memPage);
+}
+
+RC appendEmptyBlock (SM_FileHandle *fHandle)
+{
+    SM_PageHandle str = (char *)calloc(PAGE_SIZE,1); //apply for a page filled with zero bytes
+    //append the page
+    fHandle->totalNumPages+=1;
+    fHandle->curPagePos=fHandle->totalNumPages;
+    RC return_value = writeBlock(fHandle->totalNumPages, fHandle, str);
+    if (return_value!=RC_OK)
+    {
+        free(str);
+        str=NULL;
+        return return_value;
+    }
+    //change total_pages
+    rewind(fHandle->mgmtInfo);//reset file pointer
+    fprintf(fHandle->mgmtInfo,"%d\n",fHandle->totalNumPages);
+    fseek(fHandle->mgmtInfo,5+(fHandle->totalNumPages)*PAGE_SIZE,SEEK_SET); //recover file pointer
+    
+    free(str);
+    str=NULL;
+    return RC_OK;
+}
+
+RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle)
+//warning: here's assuming numberOfPages is also an offset
+{
+    if (fHandle->totalNumPages < numberOfPages)
+    {
+        int diff = numberOfPages - fHandle->totalNumPages;
+        RC return_value;
+        for (int i=0; i < diff; i++)
+        {
+            return_value = appendEmptyBlock(fHandle);
+            if (return_value!=RC_OK)
+                return return_value;
+        }
+    }
+    return RC_OK;
+}
+
 int main(void)
 {
-    int a=sizeof(char);
-    printf("%d",a);
+    printf("%c",'\0');
 }
